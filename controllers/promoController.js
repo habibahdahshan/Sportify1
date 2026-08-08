9d562891c3f569d3a736fc90b3873760ea5e865f
rahma2408851-jpg <rahma2408851@miuegypt.edu.eg>
2026-08-08 16:56:36 +0300

    Update promoController.js

--- controllers/promoController.js
+++ controllers/promoController.js
@@ -1,7 +1,8 @@
 const PromoCode = require("../models/PromoCode");
-
+  //returns a clean promo  // raw: ay code:url or query or whatever
 function normalizeCode(raw) {
   return String(raw || "")
+         //space_ zerowidthchar_         global
     .replace(/[\s\u200b-\u200d\ufeff\u00a0]/g, "")
     .toUpperCase();
 }
@@ -12,7 +13,7 @@ exports.validatePromoCode = async (req, res, next) => {
     if (!code) {
       return res.status(400).json({ success: false, message: "Code is required." });
     }
-
+          //just one 3shan promo lazm yb2a unique
     const promo = await PromoCode.findOne({ code, active: true });
     if (!promo) {
       return res.status(404).json({ success: false, message: "Invalid or inactive promo code." });
@@ -23,7 +24,7 @@ exports.validatePromoCode = async (req, res, next) => {
     next(err);
   }
 };
-
+//de msh fe routes
 exports.lookupActivePromo = async function lookupActivePromo(code) {
   const normalized = normalizeCode(code);
   if (!normalized) return null;
@@ -31,3 +32,4 @@ exports.lookupActivePromo = async function lookupActivePromo(code) {
   if (!promo) return null;
   return { code: promo.code, discountPercent: promo.discountPercent };
 };
+// de API endpoint, called by js , checkout page
